6b851dcdc460bd7a84623fdb6f68d6da17e2915a
barok-dell <bebifirew@gmail.com>
2022-07-05 12:36:56 -0700

    nestedloops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 /**
  * main - prints _putchar, fllowd by a new linees
  * Return: Alwys 0 (Success)
-*/
+ */
 int main(void)
 {
-char bar[] = "_putchar";
+char abe[] = "_putchar";
 int c;
 for (c = 0; c < 8; c++)
 {
-_putchar(bar[c]);
+_putchar(abe[c]);
 }
 _putchar('\n');
 return (0);
