09ec2e4eaefe1cd78b3fdd6d8508078c1403ad68
barok-dell <bebifirew@gmail.com>
2022-07-07 04:52:24 -0700

    changessome

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _isupper - Checks for uppercase characters
 * @c: Number to be checked
@@ -7,7 +8,8 @@
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
+{
 return (1);
-else
+}
 return (0);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#includ "_putchar.c"
+/**
+* print_numbers - Prints 0 to 9
+* Return: The numbers 0 - 9
+*/
+void print_numbers(void)
+{
+int i;
+for (i=0; i < 10; i++)
+{
+_putchar(i+"0")
+}
+_putchar('\n')
+}
